e70fb163c072edbb77a985bdc7d0a0a7823083dd
Ian Langworth <ian@langworth.com>
2022-09-17 12:08:03 -0700

    Add Chakra-UI, Recoil, and SWR

---
+++ pages/_app.tsx
@@ -0,0 +1,30 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import { NextPage } from "next"
+import { AppProps } from "next/app"
+import { ReactElement, ReactNode } from "react"
+import { RecoilRoot } from "recoil"
+import { SWRConfig } from "swr"
+
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  return (
+    <>
+      <ChakraProvider resetCSS>
+        <SWRConfig>
+          <RecoilRoot>
+            <Component {...pageProps} />
+          </RecoilRoot>
+        </SWRConfig>
+      </ChakraProvider>
+    </>
+  )
+}
+
+export default MyApp
